8a3eeeb7d126d1ae0c138ed9e3c86841f6f5aa51
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-01-22 21:31:10 -0500

    Added mouse events to canvas. When user clicks, a rectangle is drawn onto the canvas

--- video_player/main.c
+++ video_player/main.c
@@ -1,6 +1,11 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 
+static int pi = 0;
+static int brush_size = 4;
+
+static int points[100][2];
+
 void draw(GtkDrawingArea *canvas, cairo_t *cr, int width, int height,
           gpointer data) {
 	GtkStyleContext *context;
@@ -16,6 +21,19 @@ void draw(GtkDrawingArea *canvas, cairo_t *cr, int width, int height,
 
 	cairo_set_source_surface(cr, demo_image, 0, 0);
 	cairo_paint(cr);
+
+	for (int i = 0; i < pi; i++) {
+		GdkRectangle drawRect;
+		int cx = points[i][0], cy = points[i][1];
+		drawRect.x = cx - brush_size;
+		drawRect.y = cy - brush_size;
+		drawRect.width = brush_size*2;
+		drawRect.height = brush_size*2;
+
+		cairo_set_source_rgb(cr, 1, 0, 1);
+		gdk_cairo_rectangle(cr, &drawRect);
+		cairo_fill(cr);
+	}
 }
 
 void open_dialog_response(GtkNativeDialog *dialog, int response) {
@@ -39,9 +57,17 @@ void open_clicked(GtkWidget *button, GtkWindow *parent) {
 
 	gtk_native_dialog_show(GTK_NATIVE_DIALOG(dialog));
 
-
 	g_signal_connect(dialog, "response", G_CALLBACK(open_dialog_response),
 	                 NULL);
+}
+
+void canvas_clicked(GtkGestureClick *gesture, int n, double x, double y,
+                    GtkWidget *canvas) {
+	points[pi][0] = (int)x;
+	points[pi][1] = (int)y;
+	pi++;
+	gtk_widget_queue_draw(canvas);
+	printf("added %.2f %.2f to list\n", x, y);
 }
 
 GtkWidget *make_toolbar(GtkWidget *parent) {
@@ -83,6 +109,12 @@ void activate(GtkApplication *app, gpointer user_data) {
 	gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(canvas), draw, NULL,
 	                               NULL);
 	gtk_box_append(GTK_BOX(box), canvas);
+
+	// ------- MOUSE GESTURE -------
+	GtkGesture *clickGesture = gtk_gesture_click_new();
+	gtk_widget_add_controller(canvas, GTK_EVENT_CONTROLLER(clickGesture));
+	g_signal_connect(clickGesture, "pressed", G_CALLBACK(canvas_clicked),
+	                 canvas);
 
 	gtk_widget_show(window);
 }
