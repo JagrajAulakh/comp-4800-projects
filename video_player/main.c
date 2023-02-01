f23eeabba15e0c8257dce4083c17f4415cf5e5e3
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-02-01 12:44:19 -0500

    Ditched template, rewrote everything except pgm save. Only saves first 10 frames

--- video_player/main.c
+++ video_player/main.c
@@ -1,159 +1,99 @@
 #include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 #include <stdio.h>
-
-#define INBUF_SIZE 4096
+#include <sys/stat.h>
+#include <unistd.h>
 
 static void pgm_save(unsigned char *buf, int wrap, int xsize, int ysize,
-                     char *filename) {
+                     const char *directory, int frame_number) {
 	FILE *f;
 	int i;
 
+	char filename[200];
+	sprintf(filename, "%s/%04d.pgm", directory, frame_number);
 	f = fopen(filename, "wb");
 	fprintf(f, "P5\n%d %d\n%d\n", xsize, ysize, 255);
 	for (i = 0; i < ysize; i++) fwrite(buf + i * wrap, 1, xsize, f);
 	fclose(f);
 }
 
-static void decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt,
-                   const char *filename) {
-	char buf[1024];
-	int ret;
-
-	ret = avcodec_send_packet(dec_ctx, pkt);
-	if (ret < 0) {
-		fprintf(stderr, "Error sending a packet for decoding\n");
-		exit(1);
-	}
-
-	while (ret >= 0) {
-		ret = avcodec_receive_frame(dec_ctx, frame);
-		if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-			return;
-		else if (ret < 0) {
-			fprintf(stderr, "Error during decoding\n");
-			exit(1);
-		}
-
-		printf("saving frame %3d\n", dec_ctx->frame_number);
-		fflush(stdout);
-
-		/* the picture is allocated by the decoder. no need to
-		   free it */
-		snprintf(buf, sizeof(buf), "%s-%d", filename,
-		         dec_ctx->frame_number);
-		pgm_save(frame->data[0], frame->linesize[0], frame->width,
-		         frame->height, buf);
-	}
-}
-
 int main(int argc, char **argv) {
-	const char *filename, *outfilename;
-	const AVCodec *codec;
-	AVCodecParserContext *parser;
-	AVCodecContext *c = NULL;
-	FILE *f;
-	AVFrame *frame;
-	uint8_t inbuf[INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
-	uint8_t *data;
-	size_t data_size;
-	int ret;
-	int eof;
-	AVPacket *pkt;
-
 	if (argc <= 2) {
 		fprintf(stderr,
-		        "Usage: %s <input file> <output file>\n"
-		        "And check your input file is encoded by mpeg1video "
-		        "please.\n",
+		        "Usage: %s <input file> <output directory>\n"
+		        "THIS WILL OUTPUT ALL FRAMES OF THE VIDEO: BEWARE!\n",
 		        argv[0]);
 		exit(0);
 	}
-	filename = argv[1];
-	outfilename = argv[2];
+	const char *filename = argv[1], *outdir = argv[2];
 
-	pkt = av_packet_alloc();
-	if (!pkt) exit(1);
-
-	/* set end of buffer to 0 (this ensures that no overreading happens for
-	 * damaged MPEG streams) */
-	memset(inbuf + INBUF_SIZE, 0, AV_INPUT_BUFFER_PADDING_SIZE);
-
-	/* find the MPEG-1 video decoder */
-	codec = avcodec_find_decoder(AV_CODEC_ID_MPEG1VIDEO);
-	if (!codec) {
-		fprintf(stderr, "Codec not found\n");
-		exit(1);
+	if (mkdir(outdir, 0777) < 0 && errno != EEXIST) {
+		printf("Error making outputdir, error=%d, EEXIST=%d\n", errno,
+		       EEXIST);
+		return EXIT_FAILURE;
 	}
+	AVFormatContext *fmt_ctx = NULL;
+	// Open video file
+	avformat_open_input(&fmt_ctx, filename, NULL, NULL);
+	// Retrieve the stream info and write it fmt_ctx.
+	avformat_find_stream_info(fmt_ctx, NULL);
 
-	parser = av_parser_init(codec->id);
-	if (!parser) {
-		fprintf(stderr, "parser not found\n");
-		exit(1);
-	}
+	// Find the video stream index
+	const AVCodec *codec = NULL;
+	int video_stream_idx = -1;
+	video_stream_idx =
+	    av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
+	printf("Found video stream at index %d, codec is %s\n",
+	       video_stream_idx, codec->name);
 
-	c = avcodec_alloc_context3(codec);
-	if (!c) {
-		fprintf(stderr, "Could not allocate video codec context\n");
-		exit(1);
-	}
+	AVCodecContext *codec_ctx = NULL;
+	codec_ctx = avcodec_alloc_context3(codec);
 
-	/* For some codecs, such as msmpeg4 and mpeg4, width and height
-	   MUST be initialized there because this information is not
-	   available in the bitstream. */
+	avcodec_parameters_to_context(
+	    codec_ctx, fmt_ctx->streams[video_stream_idx]->codecpar);
+	avcodec_open2(codec_ctx, codec, NULL);
 
-	/* open it */
-	if (avcodec_open2(c, codec, NULL) < 0) {
-		fprintf(stderr, "Could not open codec\n");
-		exit(1);
-	}
+	AVFrame *frame = av_frame_alloc();
+	AVPacket *packet = av_packet_alloc();
 
-	f = fopen(filename, "rb");
-	if (!f) {
-		fprintf(stderr, "Could not open %s\n", filename);
-		exit(1);
-	}
-
-	frame = av_frame_alloc();
-	if (!frame) {
-		fprintf(stderr, "Could not allocate video frame\n");
-		exit(1);
-	}
-
-	do {
-		/* read raw data from the input file */
-		data_size = fread(inbuf, 1, INBUF_SIZE, f);
-		if (ferror(f)) break;
-		eof = !data_size;
-
-		/* use the parser to split the data into frames */
-		data = inbuf;
-		while (data_size > 0 || eof) {
-			ret = av_parser_parse2(
-			    parser, c, &pkt->data, &pkt->size, data, data_size,
-			    AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
-			if (ret < 0) {
-				fprintf(stderr, "Error while parsing\n");
-				exit(1);
+	int frame_number = 0;
+	// Read packets from the video file
+	while (av_read_frame(fmt_ctx, packet) >= 0) {
+		// If a packet is apart of the video stream index we are
+		// interested in:
+		if (packet->stream_index == video_stream_idx) {
+			// Send the packet to the decoder
+			int result = avcodec_send_packet(codec_ctx, packet);
+			// Error in decoding:
+			if (result < 0) {
+				return -1;
 			}
-			data += ret;
-			data_size -= ret;
 
-			if (pkt->size)
-				decode(c, frame, pkt, outfilename);
-			else if (eof)
-				break;
+			while (result >= 0) {
+				result =
+				    avcodec_receive_frame(codec_ctx, frame);
+				if (result == AVERROR(EAGAIN)) {
+					break;
+				} else if (result == AVERROR_EOF) {
+					printf("%d ERROR EOF\n", frame_number);
+					break;
+				} else if (result < 0) {
+					return -1;
+				}
+				pgm_save(frame->data[0], frame->linesize[0],
+				         frame->width, frame->height, outdir,
+				         frame_number++);
+				if (frame_number > 10) goto done;
+			}
 		}
-	} while (!eof);
+	}
 
-	/* flush the decoder */
-	decode(c, frame, NULL, outfilename);
+done:
 
-	fclose(f);
+	printf("joe\n");
 
-	av_parser_close(parser);
-	avcodec_free_context(&c);
-	av_frame_free(&frame);
-	av_packet_free(&pkt);
+	avformat_close_input(&fmt_ctx);
+	avcodec_free_context(&codec_ctx);
 
 	return 0;
 }
