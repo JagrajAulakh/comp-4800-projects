3ad923743d63013e8cc4aa735bff9136507e7544
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-02-08 17:41:28 -0500

    Removed old animation and timer code, making new demo with rick morty background, drawing two eyes

--- video_player/main.c
+++ video_player/main.c
@@ -3,111 +3,55 @@
 #include <pthread.h>
 #include <stdio.h>
 
+
+typedef struct {
+	int x, y;
+} Eye;
+
 static GtkWidget *drawingArea;
-static GTimer *timer;
-static cairo_surface_t *image1, *image2;
+static cairo_surface_t *canvas, *backgroundImage;
 
-const unsigned long duration = 800;
+static int mx, my;
+static Eye eyes[4];
 
-void setWidgetCss(GtkWidget *widget, const char *css) {
-	GtkCssProvider *provider = gtk_css_provider_new();
-	const char *data = css;
-	gtk_css_provider_load_from_data(provider, data, -1);
-	GdkDisplay *displej = gtk_widget_get_display(GTK_WIDGET(widget));
-	gtk_style_context_add_provider_for_display(
-	    displej, GTK_STYLE_PROVIDER(provider),
-	    GTK_STYLE_PROVIDER_PRIORITY_USER);
+double getAngle(double x1, double y1, double x2, double y2) {
+	return atan((y2-y1)/(x2-x1));
 }
 
 void draw(GtkDrawingArea *drawingArea, cairo_t *cr, int width, int height,
           gpointer data) {
-	if (image1 != NULL && image2 != NULL) {
-		cairo_set_source_surface(cr, image1, 0, 0);
-		cairo_paint(cr);
+	cairo_set_source_surface(cr, backgroundImage, 0, 0);
+	cairo_paint(cr);
 
-		static double percentage = 0.0, smooth_percentage1 = 0.0,
-		       smooth_percentage2 = 0.0;
-		if (g_timer_is_active(timer)) {
-			percentage =
-			    g_timer_elapsed(timer, NULL) * 1000 / duration;
-			smooth_percentage1 =
-			    (1 - cos(percentage * M_PI)) / 2;          // cosine
-			smooth_percentage2 = percentage * percentage;  // x^2
-		}
-
-		int total_width = cairo_image_surface_get_width(image1);
-		int total_height = cairo_image_surface_get_height(image1);
-		const int boxes_x = 5;
-		const int boxes_y = 2;
-
-		int stride_x = total_width / boxes_x;
-		int stride_y = total_height / boxes_y;
-
-		cairo_set_source_surface(cr, image2, 0, 0);
-		for (int bx = 0; bx < boxes_x; bx++) {
-			for (int by = 0; by < boxes_y; by++) {
-				cairo_reset_clip(cr);
-				int px = stride_x * percentage;
-				int py = stride_y * percentage;
-				cairo_rectangle(cr,
-				bx*stride_x+stride_x/2-px/2,
-				by*stride_y+stride_y/2-py/2, px, py);
-				// cairo_arc(cr, bx * stride_x + stride_x / 2,
-				//           by * stride_y + stride_y / 2,
-				//           (px < py ? px : py)/2, 0, 2 * M_PI);
-				cairo_clip(cr);
-				cairo_paint(cr);
-			}
-		}
+	cairo_set_source_rgb(cr, 1, 0, 1);
+	// Draw eyes
+	for (int i = 0; i < 2; i++) {
+		Eye eye = eyes[i];
+		cairo_arc(cr, eye.x, eye.y, 30, 0, 2 * M_PI);
+		cairo_fill(cr);
 	}
 }
 
-void *drawing_loop(void *d) {
-	unsigned long duration = *((unsigned long *)d);
-	double elapsed_time = 0;
+// void *drawing_loop(void *d) {
+// 	unsigned long duration = *((unsigned long *)d);
+// 	double elapsed_time = 0;
+//
+// 	if (timer == NULL) {
+// 		fprintf(stderr, "Timer is null\n");
+// 		pthread_exit(NULL);
+// 	}
+//
+// 	while ((elapsed_time * 1000) < duration) {
+// 		elapsed_time = g_timer_elapsed(timer, NULL);
+// 	}
+// 	g_timer_stop(timer);
+//
+// 	pthread_exit(NULL);
+// }
 
-	if (timer == NULL) {
-		fprintf(stderr, "Timer is null\n");
-		pthread_exit(NULL);
-	}
-
-	while ((elapsed_time * 1000) < duration) {
-		elapsed_time = g_timer_elapsed(timer, NULL);
-	}
-	g_timer_stop(timer);
-
-	pthread_exit(NULL);
-}
-
-void start_timer_click_callback(GtkWidget *button, gpointer data) {
-	if (g_timer_is_active(timer)) {
-		return;
-	}
-
-	g_timer_reset(timer);
-	g_timer_start(timer);
-
-	pthread_t drawing_thread;
-
-	pthread_create(&drawing_thread, NULL, drawing_loop, (void *)&duration);
-	// pthread_join(drawing_thread, NULL);
-}
-
-GtkWidget *make_toolbar(GtkWidget *parent) {
-	GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-
-	gtk_widget_set_halign(GTK_WIDGET(box), GTK_ALIGN_START);
-	gtk_widget_set_valign(GTK_WIDGET(box), GTK_ALIGN_CENTER);
-
-	GtkWidget *startTimerButton;
-
-	startTimerButton = gtk_button_new_with_label("Start timer");
-	g_signal_connect(startTimerButton, "clicked",
-	                 G_CALLBACK(start_timer_click_callback), NULL);
-
-	gtk_box_append(GTK_BOX(box), startTimerButton);
-
-	return box;
+void mouse_motion(GtkWidget widget, double x, double y, gpointer data) {
+	mx = (int)x;
+	my = (int)y;
 }
 
 void activate(GtkApplication *app, gpointer user_data) {
@@ -124,30 +68,33 @@ void activate(GtkApplication *app, gpointer user_data) {
 	gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
 	gtk_window_set_child(GTK_WINDOW(window), box);
 
-	// ------- TOOLBAR -------
-	GtkWidget *toolbar = make_toolbar(window);
-	gtk_box_append(GTK_BOX(box), toolbar);
-
 	// ------- CANVAS -------
+	canvas = cairo_image_surface_create(CAIRO_FORMAT_RGB24, 800, 600);
 	gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawingArea), draw,
 	                               NULL, NULL);
 	gtk_box_append(GTK_BOX(box), drawingArea);
 
 	// ------- LOAD IMAGES -------
-	image1 = cairo_image_surface_create_from_png("./assets/wallpaper1.png");
-	image2 = cairo_image_surface_create_from_png("./assets/wallpaper2.png");
-	gtk_widget_set_size_request(drawingArea,
-	                            cairo_image_surface_get_width(image1),
-	                            cairo_image_surface_get_height(image1));
+	backgroundImage =
+	    cairo_image_surface_create_from_png("./assets/rickmorty_blue.png");
 
-	// ------- MAKE TIMER -------
-	timer = g_timer_new();
-	g_timer_stop(timer);
-	g_timer_reset(timer);
+	gtk_widget_set_size_request(
+	    drawingArea, cairo_image_surface_get_width(backgroundImage),
+	    cairo_image_surface_get_height(backgroundImage));
 
-	g_timeout_add(1000 / 60, (GSourceFunc)gtk_widget_queue_draw,
-	              drawingArea);
-	gtk_widget_queue_draw(drawingArea);
+	// ------- MOUSE GESTURE -------
+	GtkEventController *mouseMotionController =
+	    gtk_event_controller_motion_new();
+	gtk_widget_add_controller(window, mouseMotionController);
+	g_signal_connect(mouseMotionController, "motion",
+	                 G_CALLBACK(mouse_motion), NULL);
+
+	// ------- EYES -------
+	eyes[0].x = 627;
+	eyes[0].y = 383;
+	eyes[1].x = 700;
+	eyes[1].y = 383;
+
 	gtk_widget_show(window);
 }
 
