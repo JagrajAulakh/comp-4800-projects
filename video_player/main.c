cac7d75e1f38aaecce43112eb01ae8b36a0f9f1e
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-02-08 03:03:15 -0500

    Basic animation using cairo

---
+++ video_player/main.c
@@ -0,0 +1,152 @@
+#include <gtk/gtk.h>
+#include <math.h>
+#include <pthread.h>
+#include <stdio.h>
+
+static GtkWidget *drawingArea;
+static GTimer *timer;
+static cairo_surface_t *image1, *image2;
+
+const unsigned long duration = 1500;
+
+void setWidgetCss(GtkWidget *widget, const char *css) {
+	GtkCssProvider *provider = gtk_css_provider_new();
+	const char *data = css;
+	gtk_css_provider_load_from_data(provider, data, -1);
+	GdkDisplay *displej = gtk_widget_get_display(GTK_WIDGET(widget));
+	gtk_style_context_add_provider_for_display(
+	    displej, GTK_STYLE_PROVIDER(provider),
+	    GTK_STYLE_PROVIDER_PRIORITY_USER);
+}
+
+void draw(GtkDrawingArea *drawingArea, cairo_t *cr, int width, int height,
+          gpointer data) {
+	if (image1 != NULL && image2 != NULL) {
+		cairo_set_source_surface(cr, image1, 0, 0);
+		cairo_paint(cr);
+
+		double percentage = 1.0, smooth_percentage1 = 1.0, smooth_percentage2 = 1.0;
+		if (g_timer_is_active(timer)) {
+			percentage = g_timer_elapsed(timer,NULL)*1000 / duration;
+			smooth_percentage1 = (1-cos(percentage * M_PI))/2;
+			smooth_percentage2 = percentage*percentage;
+		}
+		cairo_set_source_rgb(cr, 1, 0, 0);
+		cairo_rectangle(cr, cairo_image_surface_get_width(image1)/2-50*smooth_percentage1, cairo_image_surface_get_height(image1)/2-50*smooth_percentage1, 100*smooth_percentage1, 100*smooth_percentage1);
+		cairo_fill(cr);
+
+		// cairo_new_sub_path(cr);
+		cairo_set_source_rgb(cr, 0, 0, 1);
+		cairo_move_to(cr, 200, 200);
+		cairo_arc(cr, 200, 200, 50, 2*M_PI*smooth_percentage2, 2*M_PI*percentage);
+		cairo_fill(cr);
+
+	}
+	
+}
+
+void *drawing_loop(void *d) {
+	unsigned long duration = *((unsigned long *)d);
+	double elapsed_time = 0;
+
+	printf("got duration: %lu\n", duration);
+	if (timer == NULL) {
+		fprintf(stderr, "Timer is null\n");
+		pthread_exit(NULL);
+	}
+
+	while ((elapsed_time*1000) < duration) {
+		elapsed_time = g_timer_elapsed(timer, NULL);
+	}
+	g_timer_stop(timer);
+
+	puts("KILLING THREAD");
+	pthread_exit(NULL);
+}
+
+void start_timer_click_callback(GtkWidget *button, gpointer data) {
+	if (g_timer_is_active(timer)) {
+		return;
+	}
+
+	g_timer_reset(timer);
+	g_timer_start(timer);
+
+	pthread_t drawing_thread;
+
+	pthread_create(&drawing_thread, NULL, drawing_loop,
+	                                (void *)&duration);
+	// pthread_join(drawing_thread, NULL);
+
+}
+
+GtkWidget *make_toolbar(GtkWidget *parent) {
+	GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+
+	gtk_widget_set_halign(GTK_WIDGET(box), GTK_ALIGN_START);
+	gtk_widget_set_valign(GTK_WIDGET(box), GTK_ALIGN_CENTER);
+
+	GtkWidget *startTimerButton;
+
+	startTimerButton = gtk_button_new_with_label("Start timer");
+	g_signal_connect(startTimerButton, "clicked",
+	                 G_CALLBACK(start_timer_click_callback), NULL);
+
+	gtk_box_append(GTK_BOX(box), startTimerButton);
+
+	return box;
+}
+
+void activate(GtkApplication *app, gpointer user_data) {
+	GtkWidget *window, *box;
+
+	window = gtk_application_window_new(app);
+	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	drawingArea = gtk_drawing_area_new();
+
+	// ------- WINDOW -------
+	gtk_window_set_title(GTK_WINDOW(window), "Main application window");
+	// ------- BOX -------
+	gtk_widget_set_halign(box, GTK_ALIGN_START);
+	gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
+	gtk_window_set_child(GTK_WINDOW(window), box);
+
+	// ------- TOOLBAR -------
+	GtkWidget *toolbar = make_toolbar(window);
+	gtk_box_append(GTK_BOX(box), toolbar);
+
+	// ------- CANVAS -------
+	gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawingArea), draw,
+	                               NULL, NULL);
+	gtk_box_append(GTK_BOX(box), drawingArea);
+
+	// ------- LOAD IMAGES -------
+	image1 = cairo_image_surface_create_from_png("./assets/wallpaper1.png");
+	image2 = cairo_image_surface_create_from_png("./assets/wallpaper2.png");
+	gtk_widget_set_size_request(drawingArea,
+	                            cairo_image_surface_get_width(image1),
+	                            cairo_image_surface_get_height(image1));
+
+	// ------- MAKE TIMER -------
+	timer = g_timer_new();
+	g_timer_stop(timer);
+	g_timer_reset(timer);
+
+	g_timeout_add(1000/60, (GSourceFunc)gtk_widget_queue_draw, drawingArea);
+	gtk_widget_queue_draw(drawingArea);
+	gtk_widget_show(window);
+}
+
+int main(int argc, char *argv[]) {
+	GtkApplication *app;
+	int status;
+
+	app = gtk_application_new("org.bigbois.example",
+	                          G_APPLICATION_DEFAULT_FLAGS);
+	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+	status = g_application_run(G_APPLICATION(app), argc, argv);
+
+	g_object_unref(app);
+
+	return status;
+}
