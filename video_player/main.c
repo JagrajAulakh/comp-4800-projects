89b16e7d4aa89cec04e437f17cf267935d847ce0
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-02-23 15:35:42 -0500

    Made decode and drawing threads (producer and consumer)
    Added async queue to cache frames between two threads,
    decoding thread will add frames to the buffer while the draw thread will pop and free them

--- video_player/main.c
+++ video_player/main.c
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <pthread.h>
+
+#define CACHE_SIZE 100
 
 typedef struct {
 	AVFormatContext *fmt_ctx;
@@ -13,6 +16,8 @@ typedef struct {
 	const AVCodec *video_codec;
 	int video_stream_index;
 } VideoInfo;
+
+GAsyncQueue *frame_cache = NULL;
 
 void setWidgetCss(GtkWidget *widget, const char *css) {
 	GtkCssProvider *provider = gtk_css_provider_new();
@@ -28,7 +33,7 @@ void setWidgetCss(GtkWidget *widget, const char *css) {
 static void yuvFrameToRgbFrame(AVFrame *inputFrame, AVFrame *outputFrame) {
 	struct SwsContext *sws_ctx = sws_getContext(
 	    inputFrame->width, inputFrame->height, inputFrame->format,
-	    inputFrame->width, inputFrame->height, AV_PIX_FMT_RGBA, SWS_BICUBIC,
+	    inputFrame->width, inputFrame->height, AV_PIX_FMT_RGB32, SWS_BICUBIC,
 	    NULL, NULL, NULL);
 
 	// Set the outputFrame's configuration options
@@ -71,63 +76,123 @@ void freeVideoInfo(VideoInfo *vi) {
 	free(vi);
 }
 
-int outputFrames(const char *videoFilename, const char *outdir, int target_frame) {
-	VideoInfo *videoInfo = getVideoInfo(videoFilename);
-	// Find the video stream index
+void *decode_frames(void *arg) {
+	const char *videoFilename = (char *)arg;
+	VideoInfo *vi = getVideoInfo(videoFilename);
+	printf("Found video stream at index %d, codec is %s\n", vi->video_stream_index, vi->video_codec->name);
 
-	printf("Found video stream at index %d, codec is %s\n", videoInfo->video_stream_index, videoInfo->video_codec->name);
-
-	AVFrame *frame = av_frame_alloc();
 	AVPacket *packet = av_packet_alloc();
 
 	int frame_number = 0;
 	// Read packets from the video file
-	// while (av_read_frame(fmt_ctx, packet) >= 0) {
-	// 	// If a packet is apart of the video stream index we are
-	// 	// interested in:
-	// 	if (packet->stream_index == video_stream_idx) {
-	// 		// Send the packet to the decoder
-	// 		int result = avcodec_send_packet(codec_ctx, packet);
-	// 		// Error in decoding:
-	// 		if (result < 0) {
-	// 			return -1;
-	// 		}
-	//
-	// 		while (result >= 0) {
-	// 			result =
-	// 			    avcodec_receive_frame(codec_ctx, frame);
-	// 			if (result == AVERROR(EAGAIN)) {
-	// 				break;
-	// 			} else if (result == AVERROR_EOF) {
-	// 				printf("%d ERROR EOF\n", frame_number);
-	// 				break;
-	// 			} else if (result < 0) {
-	// 				return 0;
-	// 			}
-	//
-	// 			// Count how many frames we've seen so far
-	// 			frame_number++;
-	// 			// If we reach target frame, save it and exit
-	// 			if (frame_number == target_frame) {
-	// 				avformat_close_input(&fmt_ctx);
-	// 				avcodec_free_context(&codec_ctx);
-	// 				return 1;
-	// 			}
-	// 		}
-	// 	}
-	// }
+	while (av_read_frame(vi->fmt_ctx, packet) >= 0) {
+		// If a packet is apart of the video stream index we are
+		// interested in:
+		if (packet->stream_index == vi->video_stream_index) {
+			// Send the packet to the decoder
+			int result = avcodec_send_packet(vi->codec_context, packet);
+			// Error in decoding:
+			if (result < 0) {
+				fprintf(stderr, "ERROR DECODING PACKET\n");
+				return 0;
+			}
 
-	freeVideoInfo(videoInfo);
+			while (result >= 0) {
+				AVFrame *frame = av_frame_alloc();
+				result =
+				    avcodec_receive_frame(vi->codec_context, frame);
+				if (result == AVERROR(EAGAIN)) {
+					break;
+				} else if (result == AVERROR_EOF) {
+					printf("%d ERROR EOF\n", frame_number);
+					break;
+				} else if (result < 0) {
+					return 0;
+				}
+
+				// Count how many frames we've seen so far
+				frame_number++;
+				printf("time: %lu, calculated frame: %lu, frame_numer: %d\n", frame->best_effort_timestamp, (frame->best_effort_timestamp/256)+1, frame_number);
+
+				// Add decoded frame when safe
+				g_async_queue_lock(frame_cache);
+				g_async_queue_push_unlocked(frame_cache, frame);
+				g_async_queue_unlock(frame_cache);
+
+				while (1) {
+					g_async_queue_lock(frame_cache);
+					if (g_async_queue_length_unlocked(frame_cache) < CACHE_SIZE) {
+						g_async_queue_unlock(frame_cache);
+						break;
+					}
+					g_async_queue_unlock(frame_cache);
+				}
+			}
+		}
+	}
+
+	freeVideoInfo(vi);
+	puts("Killing decode thread");
 	return 0;
 }
 
+AVFrame *prevFrame = NULL, *prevRgbFrame = NULL;
 void draw(GtkDrawingArea *drawingArea, cairo_t *cr, int width, int height, gpointer data) {
-	cairo_set_source_rgb(cr, 1, 0, 0);
-	cairo_fill(cr);
+
+	AVFrame *frame = NULL;
+	g_async_queue_lock(frame_cache);
+	if (g_async_queue_length_unlocked(frame_cache) > 0) {
+		frame = (AVFrame *)g_async_queue_pop_unlocked(frame_cache);
+	}
+	else {
+		frame = NULL;
+	}
+	g_async_queue_unlock(frame_cache);
+
+	if (frame != NULL) {
+		AVFrame *rgbFrame = av_frame_alloc();
+		yuvFrameToRgbFrame(frame, rgbFrame);
+		cairo_surface_t *frameSurface =
+		    cairo_image_surface_create_for_data(
+		        rgbFrame->data[0], CAIRO_FORMAT_ARGB32, rgbFrame->width,
+		        rgbFrame->height, rgbFrame->linesize[0]);
+
+		cairo_set_source_surface(cr, frameSurface, 0, 0);
+		cairo_paint(cr);
+
+		if (prevFrame != NULL) {
+			av_frame_free(&prevFrame);
+		}
+		if (prevRgbFrame != NULL) {
+			av_frame_free(&prevRgbFrame);
+		}
+		prevFrame = frame;
+		prevRgbFrame = rgbFrame;
+	}
+	else {
+		cairo_rectangle(cr, 0, 0, width, height);
+		cairo_set_source_rgb(cr, 0, 0, 0);
+		cairo_fill(cr);
+	}
+}
+
+void makeDecodeThread(const char *filename) {
+	pthread_t tid;
+	pthread_create(&tid, NULL, decode_frames, (void *)filename);
 }
 
 void activate(GtkApplication *app, gpointer data) {
 	char *filename = (char *)data;
+
+	VideoInfo *vi = getVideoInfo(filename);
+	printf("Found video stream at index %d, codec is %s\n", vi->video_stream_index, vi->video_codec->name);
+
+	int framerate = vi->fmt_ctx->streams[vi->video_stream_index]->r_frame_rate.num/vi->fmt_ctx->streams[vi->video_stream_index]->r_frame_rate.den;
+	int videoWidth = vi->codec_context->width, videoHeight = vi->codec_context->height;
+	printf("Dimensions are %dx%d, framerate is %d\n", videoWidth, videoHeight, framerate);
+
+	frame_cache = g_async_queue_new();
+
 	GtkWidget *window, *box, *drawingArea;
 
 	window = gtk_application_window_new(app);
@@ -137,21 +202,15 @@ void activate(GtkApplication *app, gpointer data) {
 
 	drawingArea = gtk_drawing_area_new();
 	gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawingArea), draw, NULL, NULL);
-
-	VideoInfo *vi = getVideoInfo(filename);
-	printf("Found video stream at index %d, codec is %s\n", vi->video_stream_index, vi->video_codec->name);
-
-	int framerate = vi->fmt_ctx->streams[vi->video_stream_index]->r_frame_rate.num;
-	int videoWidth = vi->codec_context->width, videoHeight = vi->codec_context->height;
-	printf("Dimensions are %dx%d, framerate is %d\n", videoWidth, videoHeight, framerate);
+	gtk_widget_set_size_request(drawingArea, videoWidth, videoHeight);
 
 	g_timeout_add(1000/framerate, (GSourceFunc)gtk_widget_queue_draw, drawingArea);
+	makeDecodeThread(filename);
 
 	gtk_box_append(GTK_BOX(box), drawingArea);
 	gtk_window_set_child(GTK_WINDOW(window), box);
 	gtk_widget_show(window);
 
-	puts("Freeing video info");
 	freeVideoInfo(vi);
 }
 
