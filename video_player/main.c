c497d555b80580ebbaf27cb3ada18983b36ad833
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-03-19 23:07:07 -0400

    Starting to setup pulse

--- video_player/main.c
+++ video_player/main.c
@@ -1,69 +1,36 @@
-#include <gtk/gtk.h>
 #include <stdio.h>
+#include <pulse/volume.h>
+#include <pulse/context.h>
+#include <pulse/mainloop.h>
 
-// Button clicked callback function
-void click_callback(GtkWidget *widget, gpointer data) {
-	g_print("Hello World!\n");
-}
+static int i = 0;
 
-// Dropdown change callback function
-void dropdown_callback(GtkWidget *widget, gpointer data) {
-	printf("%d\n", gtk_drop_down_get_selected(GTK_DROP_DOWN(widget)));
-}
-
-void activate(GtkApplication *app, gpointer user_data) {
-	GtkWidget *window, *button, *box, *dropdown;
-
-	// Make main application window
-	window = gtk_application_window_new(app);
-	// Make a button with text
-	button = gtk_button_new_with_label("Hello World");
-	// Make a box that will hold the button
-	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	// Make a dropdown menu with predefined values
-	dropdown = gtk_drop_down_new_from_strings(
-	    (const char *[]){"joe", "mama", NULL});
-
-	// Set box alignment to centered
-	gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
-	gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
-
-	// Define callback function for button click
-	g_signal_connect(button, "clicked", G_CALLBACK(click_callback), NULL);
-	// Define callback function for button click, but pass custom arguments
-	// to callback function
-	g_signal_connect_swapped(button, "clicked",
-	                         G_CALLBACK(gtk_window_destroy), window);
-	// Call function everytime user changes dropdown value
-	g_signal_connect(dropdown, "notify::selected-item",
-	                 G_CALLBACK(dropdown_callback), NULL);
-
-	// Main window displays the box
-	gtk_window_set_child(GTK_WINDOW(window), box);
-	// Add button to box
-	gtk_box_append(GTK_BOX(box), button);
-	// Add dropdown to box
-	gtk_box_append(GTK_BOX(box), dropdown);
-
-	// Set window title
-	gtk_window_set_title(GTK_WINDOW(window), "Main application window");
-	// Define window size
-	gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-
-	// Show main window
-	gtk_widget_show(window);
+void callback(pa_context *c, void *userdata) {
+	printf("calledback %d\n", ++i);
 }
 
 int main(int argc, char *argv[]) {
-	GtkApplication *app;
-	int status;
+	pa_mainloop *m = pa_mainloop_new();
+	pa_mainloop_api *mainloop_api = pa_mainloop_get_api(m);
 
-	app = gtk_application_new("org.bigbois.example",
-	                          G_APPLICATION_DEFAULT_FLAGS);
-	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-	status = g_application_run(G_APPLICATION(app), argc, argv);
+	pa_context* pc = pa_context_new(mainloop_api, argv[0]);
 
-	g_object_unref(app);
+	if (pa_context_connect(pc, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL) < 0) {
+		fprintf(stderr, "Pulse audio context failed to connect\n");
+		goto quit;
+	}
+	puts("Connected to pulseaudio context successfully");
 
-	return status;
+
+	pa_context_set_state_callback(pc, callback, NULL);
+	
+	int ret;
+	if (pa_mainloop_run(m, &ret)) {
+        fprintf(stderr, "pa_mainloop_run() failed.\n");
+	}
+
+
+quit:
+	pa_mainloop_free(m);
+	pa_context_unref(pc);
 }
